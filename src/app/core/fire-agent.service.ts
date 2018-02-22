05d4812a2bb02896fc5e7ee83cb5ae82332f5eed
Ramtin Soltani <ramtinsoltani93@gmail.com>
2018-02-22 03:21:16 -0800

    Firebase initialization, credentials JSON added, models alias defined, JSON import enabled

--- src/app/core/fire-agent.service.ts
+++ src/app/core/fire-agent.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
 
+import * as firebase from 'firebase';
+import * as credentials from '../../credentials.json';
+
 @Injectable()
 export class FireAgentService {
 
+  constructor() {
 
+    firebase.initializeApp((<any>credentials).firebase);
+
+  }
 
 }
